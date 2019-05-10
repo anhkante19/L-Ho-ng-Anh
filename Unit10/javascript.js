9c634932f47d2d1e99ed45b842b8f38b0ff9bc11
lehoanganh1908 <hoanganh.antoan13@gmail.com>
2019-05-10 18:41:42 +0700

    homeworkU10kodaydu

---
+++ Unit10/javascript.js
@@ -0,0 +1,7 @@
+document.getElementsByTagName('a')[0].addEventListener('click',function(){
+	var item= document.getElementById('muc')
+	for (var i = muc.length - 1; i >= 0; i--) {
+		muc[i].style.display = 'block'
+		
+		}
+})
